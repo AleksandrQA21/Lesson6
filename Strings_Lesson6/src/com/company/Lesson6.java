8083396f64496bca2e84f91c3d2487f06b89a2d7
Sasha <aleksandr.titovskoy@gmail.com>
2022-01-19 20:11:14 +0200

    StringMethods clas added

---
+++ Strings_Lesson6/src/com/company/Lesson6.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Lesson6 {
+    public static void main(String[] args) {
+        String s = "Some Text String";
+        String s1 = "sometextstring";
+
+        System.out.println(s.equals(s1));
+        System.out.println(s.equalsIgnoreCase(s1));
+    }
+}
